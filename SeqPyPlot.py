9dccc2ecf65ffebe7f621a24c2c256428067c3d3
Paul Gradie <paul.e.gradie@gmail.com>
2017-02-14 15:57:16 +1100

    Revised files for SeqPyPlot (future: SeqPylot

--- SeqPyPlot.py
+++ SeqPyPlot.py
@@ -1,32 +1,33 @@
 import time
 import os
 import sys
+import SeqPyPlotLib.DataContainer as DataContainer
 
-from DataPlotter import MainDataPlotter
-from DataAnalyzer import DataAnalyzer
-from ArgumentCollector import Args
-import DataContainer
+from SeqPyPlotLib.DataPlotter import MainDataPlotter
+from SeqPyPlotLib.DataAnalyzer import DataAnalyzer
+from SeqPyPlotLib.ArgumentCollector import Args
 
 if __name__ == '__main__':
 
     start = float(time.time())
-
-    # TODO implement args.makelogs()
+    print '\n\n'
     # Collect args
-    args = Args()
-    args.make_logs()
+    argz = Args()
+    argz.make_logs()
+    args = argz.args
+
 
     # if files aren't provided properly
-    if args.args.raw_data == args.args.plot_data is None:
-        print("Provide either a (raw_data file) or (plot_data file)")
+    if args.raw_data == args.plot_data is None:
+        print("\nProvide either a (raw_data file) or (plot_data file)\n")
         os.system('python SeqPyPlot.py -h')
         sys.exit()
 
-    elif args.args.raw_data is not None and args.args.plot_data is not None:
+    elif args.raw_data is not None and args.plot_data is not None:
         print ("ERROR...")
         print "\nLoad either raw_data or pre-formatted plot_data. Don't load both!\n"
         sys.exit()
-    elif args.args.raw_data is not None and args.args.gene_list is not None:
+    elif args.raw_data is not None and args.gene_list is not None:
         print("ERROR...")
         print "\nRun the program on raw_data, then rerun supplying the _plotter_data.txt file.\n"
         sys.exit()
@@ -35,7 +36,7 @@ if __name__ == '__main__':
 
     # if only raw data is supplied
 
-    if args.args.raw_data is not None:
+    if args.raw_data is not None:
         FullContainer = DataContainer.DataContainer(args)  # object containing parsed raw_data
         Analyzer = DataAnalyzer(args, FullContainer)  # object used for filtering data and writing out filtered data files
 
@@ -46,10 +47,10 @@ if __name__ == '__main__':
         DataPrinter.write_de_results()
         DataPrinter.write_filtered_data()
 
-        if args.args.ercc:
+        if args.ercc:
             DataPrinter.write_ercc_data()
 
-        if args.args.tally:
+        if args.tally:
             Plot_Builder = MainDataPlotter(args, Analyzer, None)  # object used for generating plots
             Plot_Builder.de_bar('black')
             Plot_Builder.plot_tally()
@@ -57,18 +58,18 @@ if __name__ == '__main__':
 
         print(
             "Data analyzed, select genes for plotting from {}_filtered.txt and rerun the program with plot data.".format(
-                args.args.out))
+                args.out))
         # args.make_logs()
         sys.exit()
 
-    elif args.args.plot_data is not None:
-        if args.args.gene_list is not None:  # if plot data and gene list is provided
+    elif args.plot_data is not None:
+        if args.gene_list is not None:  # if plot data and gene list is provided
 
             # Create Output files
-            DataContainer.PrepareOutputDirectory.make_folder(args.args.out)
+            DataContainer.PrepareOutputDirectory.make_folder(args.out)
 
             FullContainer = DataContainer.DataContainer(args)  # object containing parsed premade plot data
-            if args.args.de_results is not None:  # if no results are provided, calculate them and then load them
+            if args.de_results is not None:  # if no results are provided, calculate them and then load them
                 FullContainer.load_results()
                 Analyzer = DataAnalyzer(args, FullContainer)
             else:
@@ -82,21 +83,34 @@ if __name__ == '__main__':
             FigureList = DataContainer.MakeFigureList(args)  # object containing gene list for plotting and other attributes
             Plot_Builder = MainDataPlotter(args, Analyzer, FigureList)  # object used for generating plots
 
-            if args.args.report:
+            if args.report:
                 DataPrinter = DataContainer.DataPrinter(args, FullContainer, Analyzer)
                 DataPrinter.write_plot_data()
                 DataPrinter.write_de_results()
                 DataPrinter.write_filtered_data()
 
-            if args.args.tally:
+            if args.tally:
                 Plot_Builder.de_bar('black')
                 Plot_Builder.plot_tally()
                 Analyzer.print_de_tallies()
 
             Plot_Builder.plot_figures()
         else:
-            print "Please supply a gene list for plotting with your plot data."
-            os.system('python SeqPyPlot.py -h')
+            # perform the DE analysis using processed plot data
+            FullContainer = DataContainer.DataContainer(args)
+            Analyzer = DataAnalyzer(args, FullContainer)
+            Analyzer.seqpyfilter()
+            Plot_Builder = MainDataPlotter(args, Analyzer, None)
+
+            DataPrinter = DataContainer.DataPrinter(args, FullContainer, Analyzer)
+            DataPrinter.write_de_results()
+            DataPrinter.write_filtered_data()
+
+            # if args.tally:
+            Plot_Builder.de_bar('black')
+            Analyzer.print_de_tallies()
+
+            print "Finished."
             sys.exit()
     else:
         os.system('python SeqPyPlot.py -h')
@@ -104,4 +118,5 @@ if __name__ == '__main__':
 
     end = float(time.time())
     print("Total Run Time: {}".format((end - start) / 60.0))
+    # args.make_logs()
 
