00afa03a25d525375dc7232d15157ce8eab76dae
Paul Gradie <paul.e.gradie@gmail.com>
2017-10-14 22:30:35 +0800

    moved file is empty funciton into, reordered module

--- SeqPyPlotLib/utils.py
+++ SeqPyPlotLib/utils.py
@@ -1,20 +1,6 @@
 import os
 
 
-def validate_fies_exist(data_paths):
-    """Validate all files in config exist"""
-    for file_name in data_paths:
-        assert os.path.isfile(file), "One or more files is not correct in the config."
-
-def check_for_even_number_of_files(self, data_paths):
-    """
-    Assert that there are an even number of file paths in the config
-    This should run AFTER the check that the the paths exist.
-    """
-    assert len(data_paths) % 2 == 0, "Even number of input files required. Use empty file if necessary."
-    assert len(self.args.time) % 2 == 0
-
-
 class PrepareOutputDirectory:
     """
     Must instantiate after args are set
@@ -29,3 +15,26 @@ class PrepareOutputDirectory:
 
         if not os.path.isdir(outdir):
             os.makedirs(outdir)
+
+
+def validate_fies_exist(data_paths):
+    """Validate all files in config exist"""
+    for file_name in data_paths:
+        assert os.path.isfile(file), "One or more files is not correct in the config."
+
+
+def check_for_even_number_of_files(self, data_paths):
+    """
+    Assert that there are an even number of file paths in the config
+    This should run AFTER the check that the the paths exist.
+    """
+    assert len(data_paths) % 2 == 0, "Even number of input files required. Use empty file if necessary."
+    assert len(self.args.time) % 2 == 0
+
+
+def file_is_empty(self, file_name):
+    """Determine if a given file is empty"""
+    if int(os.stat(file_name).st_size) == 0:  # if file is empty, record the list position
+        return file_name
+    else:
+        return False
