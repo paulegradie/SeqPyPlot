abe8f81fcf67cba7149b3e934038180a72784f27
Paul Gradie <paul.e.gradie@gmail.com>
2018-04-14 16:47:57 +1000

    configparser changes

--- SeqPyPlotLib/ConfigParse.py
+++ SeqPyPlotLib/ConfigParse.py
@@ -1,22 +1,20 @@
-import os
+from ConfigParser import ConfigParser
+from ast import literal_eval
 
 def config_parser(config_path):
     """
-    Read the file paths in the order given in the config file.
-    
-    Config should be tab deliminted with the file first, and its name second:
-    ~/path/to/file <TAB> name
-
-    Returns:
-        :list of lists: [[file]]
+    Read the provided config.ini file and parse the file paths to the data.
     """
-    with open(config_path, 'r') as conf: 
-        paths = list()
-        names = list()
-        for line in conf.readlines():
-            line = line.split('\t')
-            paths.append(line[0].strip())
-            names.append(line[1].strip())
-    
-    return paths, names
 
+    config = ConfigParser()
+    config.read(config_path)
+
+    control_data = literal_eval(config.get('data', 'controls'))
+    treated_data = literal_eval(config.get('data', 'treated'))
+
+    control_names = literal_eval(config.get('names', 'controls'))
+    treated_names = literal_eval(config.get('names', 'treated'))
+
+    dir_path = config.get('data_directory', 'dir')
+
+    return dir_path, zip(control_data, control_names), zip(treated_data, treated_names)
