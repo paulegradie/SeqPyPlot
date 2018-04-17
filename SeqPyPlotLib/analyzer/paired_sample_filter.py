3877aa7f02824f620f0dbcd73624363995c2a8fb
Paul Gradie <paul.e.gradie@gmail.com>
2018-04-18 08:54:50 +1000

    save

---
+++ SeqPyPlotLib/analyzer/paired_sample_filter.py
@@ -0,0 +1,15 @@
+
+from ..container.DataContainer import DataContainer
+
+
+
+class PairedSampleFilter(DataContainer):
+
+    def __init__(self, config, ):
+
+        super(self, PairedSampleFilter).__init__(config)    
+
+    def main_filter_process(self, )
+
+
+    def apply_fold_change(self, )
