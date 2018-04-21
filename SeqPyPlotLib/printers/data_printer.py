46e2b7201bea8d5c4622dc6356fd058d0c1a6b5a
Paul Gradie <paul.e.gradie@gmail.com>
2018-04-21 19:36:50 +1000

    linting

--- SeqPyPlotLib/printers/data_printer.py
+++ SeqPyPlotLib/printers/data_printer.py
@@ -1,20 +1,20 @@
 from utils.utils import make_default_dir
 from csv import writer
+
+
 class DataPrinter(object):
 
     def __init__(self, config_obj, container, analyzer):
-        
+
         assert config_obj.get('data_directory', 'output')
         assert config_obj.get('file_names', 'prefix')
 
         self.output_dir = os.path.join(config_obj.get('data_directory', 'output'),
                                        config_obj.get('file_names', 'prefix'))
         self.kwargs = {'sep': '\t'}
-
         #self.kwargs = {'columns': cols, 'header': header?, index: sep='\t'}
 
     def write_ercc_data(self, ercc_df):
-        
         ercc_file = "_".join([self.output_dir, "all_ercc.txt"])
         ercc_df.to_csv(ercc_file, **self.kwargs)
             
@@ -22,8 +22,7 @@ class DataPrinter(object):
         plot_data_file = "_".join([self.output_dir, 'plotter_data.txt'])
         plot_data_fd.to_csv(plot_data_file, **self.kwargs)
 
-    def write_filtered_data(self, filtered_df_list):
-        
+    def write_filtered_data(self, filtered_df_list):        
         for stage, data in zip(self.config_obj.get('names', 'times'), filtered_df_list):
             filtered_data_file = "_".join([self.output_dir, stage, self.prefix, 'filtered_data.txt'])
             data.to_csv(filtered_data_file, **self.kwargs)
