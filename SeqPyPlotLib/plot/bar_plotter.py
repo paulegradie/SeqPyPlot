8cc9d27cf9d27e24485a655d360dab7858c174bd
Paul Gradie <paul.e.gradie@gmail.com>
2018-04-22 12:42:01 +1000

    added bar plotter

---
+++ SeqPyPlotLib/plot/bar_plotter.py
@@ -0,0 +1,95 @@
+
+from base.plot_base import PlotBase
+
+class PairedBarPlot(BasePlot):
+
+    def __init__(self, config_obj):
+        
+        super(PlotBase, self).__init__()
+
+        self.config_obj = config_obj
+
+        self.labels = self.config_obj.getlist('names', 'times')
+        self.output_dir = self.create_output_directory()
+        self.prefix = self.config_obj.get('names', 'experiment_name')
+
+        self.log = self.config_obj.getfloat('params', 'log2fold')
+        self.hi = self.config_obj.getint('params', 'hi')
+        self.low = self.config_obj.getint('params', 'low')
+        self.diff = self.config_obj.getlist('params', 'diff')
+
+        plt.close()
+
+    def set_lines(self, color):
+        return  mlines.Line2D([], [], color=color)
+
+    def save_plot(self, fig):
+
+        path_ = os.path.join(self.output_dir, self.prefix)
+        file_name = "_".join([path_,"DE_Gene_by_time.png"])
+
+        fig.savefig(file_name, format='png', bbox_inches='tight')
+
+        return fig
+
+    def tidy_figure_up(self, fig, handles):
+
+        hi = 'inf' if self.hi > 99999 else self.hi
+        fig.legend(handles=[expression_upper, expression_lower, log_line, difference],
+                   labels=(["Log2: " + str(self.log),
+                            "Range: " + "-".join([str(self.low), str(hi)]),
+                            "Diff: {}, {}".format(str(int(self.diff[0])), str(int(self.diff[1])))]),
+                   loc='upper right')
+
+        return fig
+
+    def create_subplot(self, x_axis, y_values, x_label, bar_width=0.5, color='black'):
+        ax = plt.subplot()
+        ax.set_xticks(x_axis)
+        ax.set_xticklabels(x_label, rotation='vertical')
+        ax.set_ylim([0, ymax])
+        ax.bar(x_axis, y_values, bar_width, color=colour, align="center")
+
+        ax = self.format_plot(ax, num_stages=len(x_label))
+        return ax
+
+    def format_plot(self, ax, num_stages):
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('No. of Flagged Genes')
+        ax.set_xlabel('Experimental Stage')
+        ax.set_title('Number of flagged genes per stage', loc='left')
+        ax.yaxis.grid()
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(True)
+        ax.spines['left'].set_visible(True)
+
+        if num_stages > 6:
+            ax.set_tick_params(axis='both', which='major', labelsize=8)
+        else:
+            ax.set_tick_params(axis='both',  # changes apply to the x-axis
+                               which='both',  # both major and minor ticks are affected
+                               bottom='on',  # ticks along the bottom edge are off
+                               top='off',  # ticks along the top edge are off
+                               left='on',
+                               right='off',
+                               labelbottom='on')  # labels along the bottom edge are off
+
+        return ax
+
+    def create_bar_plot(self, de_count_by_time_dict):
+
+        y_values = [int(de_count_by_time_dict[x]) for x in self.labels]
+        x_label = self.labels
+        ymax = max(y_values) * 1.3
+  
+        x_axis = range(len(y_values))
+
+        fig = self.set_figure(figure_prefix='Paired Sample DE Bar Plot')
+        ax = self.create_subplot(x_axis, y_values, x_label)
+
+        line_handles = [self.set_lines('white') for _ in range(4)]
+        fig = self.tidy_figure_up(fig, line_handles)
+
+        self.save_plot(fig)
