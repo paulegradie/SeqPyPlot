e41d5e4e96b0a661a46b84c7fc6ac6bc519b78e2
Paul Gradie <paul.e.gradie@gmail.com>
2018-05-12 10:02:37 +1000

    project files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from distutils.core import setup
+
+
+
+setup(
+    name='SeqPyPlot',
+    version='0.4.0',
+    author='Paul E Gradie',
+    author_email='paul.e.gradie@gmail.com',
+    packages=['seqpyplot', 'seqpyplot.test'],
+    scripts=['bin/SeqPyPlot.py'],
+    url='http://pypi.python.org/pypi/seqpyplot',
+    license='LICENSE.txt',
+    description='Package for descriptive analysis.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pandas == 0.22.0",
+        "numpy == 1.14.3",
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+
+)
+
