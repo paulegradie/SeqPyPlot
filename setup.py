5755213ab4dffa9215cfbf0ed7b289cd596178f6
Paul <paul.e.gradie@gmail.com>
2018-05-13 10:56:50 +1000

    modified setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 # from distutils.core import setup
 
 setup(
@@ -6,16 +6,15 @@ setup(
     version='0.4.0',
     author='Paul E Gradie',
     author_email='paul.e.gradie@gmail.com',
-    packages=['seqpyplot', 'tests'],
+    packages=find_packages(exclude=['tests']),
     scripts=['bin/SeqPyPlot.py'],
     url='http://pypi.python.org/pypi/seqpyplot',
     license='LICENSE.txt',
     description='Package for descriptive analysis.',
-    long_description=open('README.txt').read(),
+    long_description=open('docs/README.txt').read(),
     install_requires=[
         "pandas == 0.22.0",
         "numpy == 1.14.3",
-        
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
